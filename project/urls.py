092b44bcc523b197766f7f38d3909c011864bad7
Richmond Lacanlalay <monde.lacanlalay@ocomsoft.com>
2020-02-26 17:48:32 +0800

    setup the navbar for home screen/page

--- project/urls.py
+++ project/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
         name="django_registration_register"),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('gasolinestation.urls')),
     path('api/v1/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/rest-auth/', include('rest_auth.urls')),
