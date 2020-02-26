c14c0c431f136f30ddb349f3befd8717f4d48144
Xednom <monde.lacanlalay@gmail.com>
2020-02-27 00:14:38 +0800

    Added the intial design for the profile screen

--- static/vue/src/users/profile.js
+++ static/vue/src/users/profile.js
@@ -1,5 +1,5 @@
 Vue.config.devtools = true;
-Vue.http.headers.common['X-CSRFToken'] = "{{ csrf_token }}";
+// Vue.http.headers.common['X-CSRFToken'] = "{{ csrf_token }}";
 // import { apiService } from "../common/api.service.js";
 
 new Vue({
@@ -14,28 +14,14 @@ new Vue({
   },
   mounted() {
     this.fetchLoginCredentials();
-    this.fetchGasStation();
   },
   methods: {
     fetchLoginCredentials() {
       this.loading = true;
       let endpoint = "/api/v1/user/";
-      this.$http.get(endpoint)
+      axios.get(endpoint)
         .then((response) => {
           this.profiles = response.data;
-          this.loading = false;
-        })
-        .catch(err => {
-          this.loading = false;
-          console.log(err);
-        })
-    },
-    fetchGasStation() {
-      this.loading = true;
-      let endpoint = "/api/v1/gas-station/";
-      this.$http.get(endpoint)
-        .then((response) => {
-          this.gasStation = response.data;
           this.loading = false;
         })
         .catch(err => {
