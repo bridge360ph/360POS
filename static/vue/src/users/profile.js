d19b5b2498b2ed6deb675d75e1cc32d496e9e60b
Xednom <monde.lacanlalay@gmail.com>
2020-02-27 16:11:46 +0800

    fixed loading sequence in profile update and read

--- static/vue/src/users/profile.js
+++ static/vue/src/users/profile.js
@@ -12,6 +12,8 @@ new Vue({
       profiles: [],
       gasStations: [],
       loading: false,
+      viewing: false,
+      saving: false,
       currentProfile: {}
     };
   },
@@ -35,34 +37,34 @@ new Vue({
       }
     },
     updateLogin() {
-      this.loading = true;
+      this.saving = true;
       let endpoint = `/api/v1/user/${this.currentProfile.id}/`;
       if (this.profiles) {
         axios.put(endpoint, this.currentProfile)
           .then((response) => {
-            this.loading = false;
+            this.saving = false;
             this.currentProfile = response.data;
             this.fetchLoginCredentials();
 
             $("#profileModal").modal("hide")
           })
           .catch((err) => {
-            this.loading = false;
+            this.saving = false;
             console.log(err);
           })
       }
     },
     fetchLoginCredential(id) {
-      this.loading = true;
+      this.viewing = true;
       let endpoint = `/api/v1/user/${id}/`;
       if (this.profiles){
         axios.get(endpoint)
         .then((response) => {
           this.currentProfile = response.data;
-          this.loading = false;
+          this.viewing = false;
         })
         .catch(err => {
-          this.loading = false;
+          this.viewing = false;
           console.log(err);
         })
       }
