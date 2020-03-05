79e2dfd69c173bc98b86358b2d6c63f664362162
Xednom <monde.lacanlalay@gmail.com>
2020-03-05 21:56:16 +0800

    added the computations for sales

--- gasolinestation/views.py
+++ gasolinestation/views.py
@@ -1,3 +1,8 @@
+import datetime
+
+from django.db.models import Avg, Sum, Q
+from . models import TransactionSales
+
 from django.shortcuts import render
 
 from django.views.generic import TemplateView
@@ -17,8 +22,26 @@ class AddGasView(LoginRequiredMixin, TemplateView):
 
 
 class SalesView(LoginRequiredMixin, TemplateView):
+    model = TransactionSales
     template_name = 'sales/SalesTable.html'
 
+    def get(self, request):
+        today = datetime.date.today()
+        sales = TransactionSales.objects.all().aggregate(Sum('sales'))
+        today_sales = TransactionSales.objects.filter(created_at=today).aggregate(Sum('sales'))
+        liter = TransactionSales.objects.all().aggregate(Sum('dispensed_liter'))
+        today_liter = TransactionSales.objects.filter(created_at=today).aggregate(Sum('dispensed_liter'))
+        price = TransactionSales.objects.all().aggregate(Sum('price'))
+        today_price = TransactionSales.objects.filter(created_at=today).aggregate(Sum('price'))
+        ctx = {
+            'sales': sales,
+            'liter': liter,
+            'price': price,
+            'today_sales': today_sales,
+            'today_liter': today_liter,
+            'today_price': today_price
+        }
+        return render(request, self.template_name, ctx)
 
 class PriceView(LoginRequiredMixin, TemplateView):
     template_name = 'price/PriceTable.html'
