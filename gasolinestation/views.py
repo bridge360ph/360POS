bcc153974dd77a6bbbad54c7540e724aae10d163
Xednom <monde.lacanlalay@gmail.com>
2020-03-06 21:58:53 +0800

    added the filter for all computations

--- gasolinestation/views.py
+++ gasolinestation/views.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 
 from django.db.models import Avg, Sum, Q
 from . models import TransactionSales
@@ -26,20 +26,35 @@ class SalesView(LoginRequiredMixin, TemplateView):
     template_name = 'sales/SalesTable.html'
 
     def get(self, request):
-        today = datetime.date.today()
+        today = datetime.today()
+        yearly = datetime.today().year
         sales = TransactionSales.objects.all().aggregate(Sum('sales'))
         today_sales = TransactionSales.objects.filter(created_at=today).aggregate(Sum('sales'))
+        weekly_sales = TransactionSales.objects.filter(created_at__week=52).aggregate(Sum('sales'))
+        yearly_sales = TransactionSales.objects.filter(created_at__year=yearly).aggregate(Sum('sales'))
         liter = TransactionSales.objects.all().aggregate(Sum('dispensed_liter'))
         today_liter = TransactionSales.objects.filter(created_at=today).aggregate(Sum('dispensed_liter'))
+        weekly_liter = TransactionSales.objects.filter(created_at__week=52).aggregate(Sum('dispensed_liter'))
+        yearly_liter = TransactionSales.objects.filter(created_at__year=yearly).aggregate(Sum('dispensed_liter'))
         price = TransactionSales.objects.all().aggregate(Sum('price'))
         today_price = TransactionSales.objects.filter(created_at=today).aggregate(Sum('price'))
+        weekly_price = TransactionSales.objects.filter(created_at__week=52).aggregate(Sum('price'))
+        yearly_price = TransactionSales.objects.filter(created_at__year=yearly).aggregate(Sum('price'))
+
         ctx = {
             'sales': sales,
             'liter': liter,
             'price': price,
             'today_sales': today_sales,
             'today_liter': today_liter,
-            'today_price': today_price
+            'today_price': today_price,
+            'weekly_sales': weekly_sales,
+            'weekly_liter': weekly_liter,
+            'weekly_price': weekly_price,
+            'yearly_sales': yearly_sales,
+            'yearly_liter': yearly_liter,
+            'yearly_price': yearly_price
+
         }
         return render(request, self.template_name, ctx)
 
