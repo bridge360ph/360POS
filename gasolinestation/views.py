91ff2dc43cf6c8b744867c772a86b25ec6c5ea77
Xednom <monde.lacanlalay@gmail.com>
2020-03-07 18:38:18 +0800

    added computations in the overview for cashier user

--- gasolinestation/views.py
+++ gasolinestation/views.py
@@ -12,6 +12,21 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'home.html'
 
+    def get(self, request):
+        weekly_sales = TransactionSales.objects.filter(
+            created_at__week=52).aggregate(Sum('sales'))
+        weekly_liter = TransactionSales.objects.filter(
+            created_at__week=52).aggregate(Sum('dispensed_liter'))
+        weekly_price = TransactionSales.objects.filter(
+            created_at__week=52).aggregate(Sum('price'))
+        
+        ctx = {
+            'weekly_sales': weekly_sales,
+            'weekly_liter': weekly_liter,
+            'weekly_price': weekly_price
+        }
+        return render(request, self.template_name, ctx)
+
 
 class GasView(LoginRequiredMixin, TemplateView):
     template_name = 'gas/GasTable.html'
@@ -29,17 +44,26 @@ class SalesView(LoginRequiredMixin, TemplateView):
         today = datetime.today()
         yearly = datetime.today().year
         sales = TransactionSales.objects.all().aggregate(Sum('sales'))
-        today_sales = TransactionSales.objects.filter(created_at=today).aggregate(Sum('sales'))
-        weekly_sales = TransactionSales.objects.filter(created_at__week=52).aggregate(Sum('sales'))
-        yearly_sales = TransactionSales.objects.filter(created_at__year=yearly).aggregate(Sum('sales'))
+        today_sales = TransactionSales.objects.filter(
+            created_at=today).aggregate(Sum('sales'))
+        weekly_sales = TransactionSales.objects.filter(
+            created_at__week=52).aggregate(Sum('sales'))
+        yearly_sales = TransactionSales.objects.filter(
+            created_at__year=yearly).aggregate(Sum('sales'))
         liter = TransactionSales.objects.all().aggregate(Sum('dispensed_liter'))
-        today_liter = TransactionSales.objects.filter(created_at=today).aggregate(Sum('dispensed_liter'))
-        weekly_liter = TransactionSales.objects.filter(created_at__week=52).aggregate(Sum('dispensed_liter'))
-        yearly_liter = TransactionSales.objects.filter(created_at__year=yearly).aggregate(Sum('dispensed_liter'))
+        today_liter = TransactionSales.objects.filter(
+            created_at=today).aggregate(Sum('dispensed_liter'))
+        weekly_liter = TransactionSales.objects.filter(
+            created_at__week=52).aggregate(Sum('dispensed_liter'))
+        yearly_liter = TransactionSales.objects.filter(
+            created_at__year=yearly).aggregate(Sum('dispensed_liter'))
         price = TransactionSales.objects.all().aggregate(Sum('price'))
-        today_price = TransactionSales.objects.filter(created_at=today).aggregate(Sum('price'))
-        weekly_price = TransactionSales.objects.filter(created_at__week=52).aggregate(Sum('price'))
-        yearly_price = TransactionSales.objects.filter(created_at__year=yearly).aggregate(Sum('price'))
+        today_price = TransactionSales.objects.filter(
+            created_at=today).aggregate(Sum('price'))
+        weekly_price = TransactionSales.objects.filter(
+            created_at__week=52).aggregate(Sum('price'))
+        yearly_price = TransactionSales.objects.filter(
+            created_at__year=yearly).aggregate(Sum('price'))
 
         ctx = {
             'sales': sales,
@@ -57,6 +81,7 @@ class SalesView(LoginRequiredMixin, TemplateView):
 
         }
         return render(request, self.template_name, ctx)
+
 
 class PriceView(LoginRequiredMixin, TemplateView):
     template_name = 'price/PriceTable.html'
