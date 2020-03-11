4ccc1e343fad474e7a40b3e9b0f4b516c2e1e6a7
Richmond Lacanlalay <monde.lacanlalay@ocomsoft.com>
2020-03-11 19:28:20 +0800

    added a management cmd for deleting data in the fields

---
+++ api/management/commands/delete_data.py
@@ -0,0 +1,18 @@
+from gasolinestation.models import GasStations, PriceManagement
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """
+    Updates the Gas Station model's name to empty
+    (this can be enhance along the way)
+    """
+    help = 'Update string to none for gas station'
+
+    def handle(self, *args, **kwargs):
+        Transactions.objects.all().delete()
+        GasStations.objects.all().delete()
+        FuelPrices.objects.all().delete()
+        GasolineStation.objects.all().delete()
+        self.stdout.write(self.style.SUCCESS("Fields are now empty"))
